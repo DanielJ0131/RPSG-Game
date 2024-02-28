eae29f199f83e898c765651a412425eede036f66
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-02-28 16:26:43 +0100

    Fix

--- src/rpsg/scoreboard.py
+++ src/rpsg/scoreboard.py
@@ -19,17 +19,23 @@ class Scoreboard:
         print("| Player names           Win count |")
         print("===================================")
         # Sort the stats by win count.
-        stats.sort(key=lambda x: x[1], reverse=True)
-
-        for name, score in stats:
-            print(f"| {name:22} {score:<9} |")
+        if stats is None:
+            print("| -                      -         |")
+        else:
+            stats.sort(key=lambda x: x[1], reverse=True)
+            for name, score in stats:
+                print(f"| {name:22} {score:<9} |")
         print("===================================")
         print("\n")
         time.sleep(5)
 
     def save_score(self, player, score):
         """Save the scores in a binary file."""
-        score_list = [(player, score)]
+        score_list = self.get_score()  # Get the existing scores
+        if score_list is None:
+            score_list = []
+        else:
+            score_list.append((player, score))  # Append the new player score
         pickle.dump(score_list, open("src/rpsg/scoreboard.dat", "ab"))
 
     def get_score(self):
