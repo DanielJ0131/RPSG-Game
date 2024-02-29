16fead99b284ef43480818329a2cb8879c85bbf8
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-03-01 00:27:03 +0100

    Made __init__.py

--- src/rpsg/test/test_computer.py
+++ src/rpsg/test/test_computer.py
@@ -1,31 +1,37 @@
 """Test File for Unit Testing computer.py."""
+from src.rpsg.computer import Computer
 
 
-def test_get_choice(self):
-    """Test get_choice method."""
-    self.computer.set_choice(self.choice)
-    assert self.computer.get_choice() in ["rock", "paper", "scissors", "gun"]
+class ComputerTest:
+    """Test Class for Computer."""
 
+    def __init__(self):
+        """Initialization Method for ComputerTest."""
+        self.computer = Computer()
+        self.choice = None
 
-def test_set_choice(self):
-    """Test the set_choice method."""
-    self.computer.set_choice(self.choice)
-    assert self.choice in ["rock", "paper", "scissors", "gun"]
+    def test_computer(self):
+        """Test the Computer class."""
+        assert self.computer is not None
 
+    def test_get_choice(self):
+        """Test get_choice method."""
+        self.computer.set_choice()
+        assert self.computer.get_choice() in ["rock", "paper", "scissors",
+                                              "gun"]
 
-def test_easy_choice(self):
-    """Test the easy_choice method."""
-    self.computer.easy_choice()
-    assert self.computer.get_choice() == "rock"
+    def test_set_choice(self):
+        """Test the set_choice method."""
+        self.computer.set_choice()
+        assert self.choice in ["rock", "paper", "scissors", "gun"]
 
+    def test_easy_choice(self):
+        """Test the easy_choice method."""
+        self.computer.easy_choice()
+        assert self.computer.get_choice() == "rock"
 
-def test_medium_choice(self):
-    """Test the medium_choice method."""
-    self.computer.medium_choice()
-    assert self.computer.get_choice() in ["rock", "paper", "scissors", "gun"]
-
-
-def test_hard_choice(self):
-    """Test the hard_choice method."""
-    self.computer.hard_choice()
-    assert self.computer.get_choice() in ["rock", "paper", "scissors", "win"]
+    def test_hard_choice(self):
+        """Test the hard_choice method."""
+        self.computer.hard_choice()
+        assert self.computer.get_choice() in ["rock", "paper", "scissors",
+                                              "win"]
