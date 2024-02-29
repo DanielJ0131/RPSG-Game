f7a998e6174246158c924cfc23e98b5f2cbf76f5
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-02-29 22:56:48 +0100

    Fix

--- src/rpsg/scoreboard.py
+++ src/rpsg/scoreboard.py
@@ -35,6 +35,7 @@ class Scoreboard:
         if player is None:
             score_dict = None
         else:
+            score_dict = {}
             score_dict[player] = score
             pickle.dump(score_dict, open("src/rpsg/scoreboard.dat", "ab"))
 
