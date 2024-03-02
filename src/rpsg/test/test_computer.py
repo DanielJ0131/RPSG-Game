8b175d5f171fb12fe4251cd0ffa015b04b9ff646
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-03-02 16:00:37 +0100

    test_computer.py working!

--- src/rpsg/test/test_computer.py
+++ src/rpsg/test/test_computer.py
@@ -1,37 +1,43 @@
 """Test File for Unit Testing computer.py."""
+# pylint: disable=import-error
+import unittest
 from ..computer import Computer
 
 
-class ComputerTest:
+class ComputerTest(unittest.TestCase):
     """Test Class for Computer."""
 
-    def __init__(self):
-        """Initialize Method for ComputerTest."""
+    def setUp(self):
+        """Setup for the ComputerTest."""
         self.computer = Computer()
-        self.choice = None
 
     def test_computer(self):
         """Test the Computer class."""
-        assert self.computer is not None
+        self.assertIsInstance(self.computer, Computer)
 
     def test_get_choice(self):
         """Test get_choice method."""
         self.computer.set_choice()
-        assert self.computer.get_choice() in ["rock", "paper", "scissors",
-                                              "gun"]
+        self.assertTrue(isinstance(self.computer.get_choice(), str))
 
     def test_set_choice(self):
         """Test the set_choice method."""
-        self.computer.set_choice()
-        assert self.choice in ["rock", "paper", "scissors", "gun"]
+        for _ in range(100):
+            self.computer.set_choice()
+            self.assertIn(self.computer.get_choice(), ["rock", "paper",
+                                                       "scissors", "gun"])
 
     def test_easy_choice(self):
         """Test the easy_choice method."""
         self.computer.easy_choice()
-        assert self.computer.get_choice() == "rock"
+        self.assertEqual(self.computer.easy_choice(), "rock")
 
     def test_hard_choice(self):
         """Test the hard_choice method."""
-        self.computer.hard_choice()
-        assert self.computer.get_choice() in ["rock", "paper", "scissors",
-                                              "win"]
+        for _ in range(100):
+            self.computer.hard_choice()
+            self.assertIn(self.computer.hard_choice(), ["rock", "paper",
+                                                        "scissors", "win"])
+
+
+unittest.main()
