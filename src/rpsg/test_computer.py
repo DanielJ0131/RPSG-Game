29e95ec36f8ec72e4711b75c1b6da30ca7d490c2
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-03-03 13:40:41 +0100

    Fix unittest

--- src/rpsg/test_computer.py
+++ src/rpsg/test_computer.py
@@ -1,40 +1,48 @@
 """Test File for Unit Testing computer.py."""
+import unittest
 from computer import Computer
 
 
-def set_up(self):
-    """Set the ComputerTest."""
-    self.computer = Computer()
+def main():
+    """Run the test."""
+    unittest.main()
 
 
-def test_computer(self):
+class ComputerTest(unittest.TestCase):
     """Test the Computer class."""
-    self.assertIsInstance(self.computer, Computer)
 
+    def setUp(self):
+        """Set the ComputerTest."""
+        self.computer = Computer()
 
-def test_get_choice(self):
-    """Test get_choice method."""
-    self.computer.set_choice()
-    self.assertTrue(isinstance(self.computer.get_choice(), str))
+    def test_computer(self):
+        """Test the Computer class."""
+        self.assertIsInstance(self.computer, Computer)
 
-
-def test_set_choice(self):
-    """Test the set_choice method."""
-    for _ in range(100):
+    def test_get_choice(self):
+        """Test get_choice method."""
         self.computer.set_choice()
-        self.assertIn(self.computer.get_choice(), ["rock", "paper",
-                                                   "scissors", "gun"])
+        self.assertTrue(isinstance(self.computer.get_choice(), str))
+
+    def test_set_choice(self):
+        """Test the set_choice method."""
+        for _ in range(100):
+            self.computer.set_choice()
+            self.assertIn(self.computer.get_choice(), ["rock", "paper",
+                                                       "scissors", "gun"])
+
+    def test_easy_choice(self):
+        """Test the easy_choice method."""
+        self.computer.easy_choice()
+        self.assertEqual(self.computer.easy_choice(), "rock")
+
+    def test_hard_choice(self):
+        """Test the hard_choice method."""
+        for _ in range(100):
+            self.computer.hard_choice()
+            self.assertIn(self.computer.hard_choice(), ["rock", "paper",
+                                                        "scissors", "win"])
 
 
-def test_easy_choice(self):
-    """Test the easy_choice method."""
-    self.computer.easy_choice()
-    self.assertEqual(self.computer.easy_choice(), "rock")
-
-
-def test_hard_choice(self):
-    """Test the hard_choice method."""
-    for _ in range(100):
-        self.computer.hard_choice()
-        self.assertIn(self.computer.hard_choice(), ["rock", "paper",
-                                                    "scissors", "win"])
+if __name__ == "__main__":
+    main()
--- src/rpsg/test_menu.py
+++ src/rpsg/test_menu.py
@@ -11,7 +11,7 @@ def main():
 class MenuTest(unittest.TestCase):
     """Test the Menu class."""
 
-    def test_set_up(self):
+    def setUp(self):
         """Set the MenuTest."""
         self.menu = Menu()
 
