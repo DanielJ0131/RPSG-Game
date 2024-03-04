4f246fe45e2a870f8b8bd92a1cfe8157cab21a91
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-03-04 19:26:38 +0100

    Fix test_game.py

--- src/rpsg/test_game.py
+++ src/rpsg/test_game.py
@@ -1,1 +1,31 @@
 """Test File for Unit Testing game.py."""
+# pylint: disable=import-error
+import unittest
+from game import Game
+
+
+def main():
+    """Run the test."""
+    unittest.main()
+
+
+class GameTest(unittest.TestCase):
+    """Test the Game class."""
+
+    def setUp(self):
+        """Set the Game class."""
+        self.game = Game()
+
+    def test_game(self):
+        """Test the game class."""
+        self.assertIsInstance(self.game, Game)
+
+    def test_init(self):
+        """Test the __init__ method."""
+        self.assertEqual(self.game.switch, {'rock': 'scissors',
+                                            'paper': 'rock',
+                                            'scissors': 'paper'})
+
+
+if __name__ == "__main__":
+    main()
