9070a3114a7c78cfd706e3774bdc804002e6bb46
Chrise98 <159011455+Chrise98@users.noreply.github.com>
2024-03-05 21:32:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Examination2',
     version='1.0.0',
-    url='https://github.com/mypackage.git',
+    url='https://github.com/Examination2.git',
     description='Description of my package',
     packages=find_packages(),    
 )
