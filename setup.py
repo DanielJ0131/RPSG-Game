8c2f3de1ddbb2d61331d82beabd4b714847f1d90
Germanator98 <159011455+Germanator98@users.noreply.github.com>
2024-03-05 21:31:40 +0100

    new file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Examination2',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    description='Description of my package',
+    packages=find_packages(),    
+)
