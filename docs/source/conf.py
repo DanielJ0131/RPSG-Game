93633b87eb3e654070a50a9a7d570f91c61bc8f4
Daniel Jönsson <143799728+DanielJ0131@users.noreply.github.com>
2024-03-08 12:20:51 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, (Mi) Ngoc Hong, Daniel Jönsson, Chris Lubert'
 author = '(Mi) Ngoc Hong, Daniel Jönsson, Chris Lubert'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.8.0'
 
 
 # -- General configuration ---------------------------------------------------
